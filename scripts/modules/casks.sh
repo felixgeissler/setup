ba72d63d3a19f2bbd9180d6484b4043fe2ed3770
Felix Geißler <felix.geissler.94+dev@gmail.com>
2024-04-04 09:38:25 +0200

    🔧 Personalize scripts

--- scripts/modules/casks.sh
+++ scripts/modules/casks.sh
@@ -27,7 +27,7 @@ info '📝 Installing office tools'
 brew install --cask microsoft-outlook microsoft-word microsoft-excel sipgate-softphone
 
 info '🪛 Installing utility tools'
-brew install --cask clipy logi-options-plus
+brew install --cask clipy logi-options-plus disk-inventory-x
 
 info '🎧 Installing entertainment software'
 brew install --cask spotify
